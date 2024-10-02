b14d20313b42e1c001c03a2b88a926c80d4740b9
uhanik148 <bangwool148@gmail.com>
2024-10-02 21:52:53 +0700

    Latihan 1 pemrograman api

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1>TESTING</h1>
+    <br/>
+    <Link href="/posts">POSTING PAGE</Link>
+    <br/>
+    <Link href="/albums">ALBUM PAGE</Link>
+  );
+}
---
+++ app/posts/page.tsx
@@ -0,0 +1,38 @@
+import CardList from "../components/Posts/CardList"
+import ViewUserButton from "../components/Posts/ViewUserButton"
+
+const base_url = "https://jsonplaceholder.typicode.com/posts"
+
+interface Iposts {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+const Posts = async() => {
+    const response = await fetch(base_url, {
+        cache: "no-store"
+    })
+    const posts: Iposts[] = await response.json()
+    return (
+        <div>
+        <p>{new Date().toLocaleTimeString()}</p>
+        <h1 className="text-fuchsia-300">POSTINGAN PAGE</h1>
+        {posts.map((post) =>{
+            return (
+                <CardList key={post.id}>
+                <p>{post.id}</p>
+                <i>{post.title}</i>
+                <p>{post.body}</p>
+                <ViewUserButton userId={post.userId}/>
+                </CardList>
+            )
+        })}
+        
+                
+        </div>
+    )
+}
+
+export default Posts
